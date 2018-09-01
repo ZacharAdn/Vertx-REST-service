b509f4c8ec2481b10dad8d0c10003801f8a8ce69
zaharAd <zahar91@gmail.com>
2018-09-02 02:37:43 +0300

    done client server

--- src/main/java/test/project1/Server.java
+++ src/main/java/test/project1/Server.java
@@ -1,31 +1,28 @@
 package test.project1;
 
-import com.mongodb.client.model.Filters;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.FindOptions;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
-import org.bson.BsonDocument;
-import org.bson.conversions.Bson;
 
-import java.util.Comparator;
 import java.util.List;
 
 
 public class Server extends AbstractVerticle {
 
-	private Router router;
-	private MongoClient client;
+	private MongoClient mongoclient;
 	private String COLLECTION = "words";
 
 	@Override
 	public void start(Future<Void> fut) throws Exception {
 
 
-		router = Router.router(vertx);
+		Router router = Router.router(vertx);
 
 		vertx.createHttpServer().requestHandler(router::accept)
 				.listen(
@@ -46,157 +43,171 @@ public class Server extends AbstractVerticle {
 				.put("connection_string", "mongodb://localhost:27017")
 				.put("db_name", "words");
 
-		client = MongoClient.createShared(vertx, config);
+		mongoclient = MongoClient.createShared(vertx, config);
 
 		router.post("/analyze").handler(this::analyze);
 
-
-		client.find(COLLECTION, new JsonObject(), results -> {
-			List<JsonObject> objects = results.result();
-
-			for (Object object: objects.toArray()) {
-				System.out.println(object);
-//				client.removeDocument(COLLECTION, (JsonObject) object, res -> {
-
-//				});
-			}
-		});
+//		final FindOptions printOptions = new FindOptions().setSort(new JsonObject().put("word",1));
+//		mongoclient.findWithOptions(COLLECTION, new JsonObject(),printOptions, results -> {
+//			List<JsonObject> objects = results.result();
+//
+//			for (Object object: objects.toArray()) {
+//				System.out.println(object);
+////				mongoclient.removeDocument(COLLECTION, (JsonObject) object, res -> {
+//
+////				});
+//			}
+//		});
 	}
 
 
 
 	// handle anything POSTed to /analyze
 	private void analyze(RoutingContext context) {
-		JsonObject record = new JsonObject();
-		JsonObject body = context.getBodyAsJson();
-		String text = (String) body.getValue("text");
+		String text = ((String) context.getBodyAsJson().getValue("text")).toLowerCase();
+		final String[] ans = new String[2];
 
-		System.out.println("text:" + text);
-
-
-		client.find(COLLECTION, new JsonObject().put("word", text), res -> {
-			JsonObject ans = new JsonObject();
+		mongoclient.find(COLLECTION, new JsonObject().put("word", text), res -> {
 
 			if (!res.result().isEmpty()) {// the word exist in the db
-
-				System.out.println("result: " + res.result());
-				ans.put("value", text).put("lexical", text);
-				context.response().end(ans.toString()+"\n");
+				ans[0] = text;
+				ans[1] = text;
 
 			} else { // the word dosent exist in DB, look for the closest match
 
 				// calc words value
 				int wordVal = 0;
 				for (int i = 0; i < text.length(); i++) {
-					if (Character.isAlphabetic(text.charAt(i))) {
-						wordVal += ((int) Character.toUpperCase(text.charAt(i)) - 64);
+					if (Character.isAlphabetic(text.charAt(i))) { //check edge case
+						wordVal += ((int) text.charAt(i) - 96);
 					}else{
 						context.response().end("Wrong input! please type word without digits\n");
-						// need to throw exception!
+						context.fail(res.cause());
 					}
 				}
-				System.out.println("word value: " + wordVal);
-
-				// create Json object to insert the db
-				record.put("word", text).put("value", wordVal);
-
-				// query for value that greater than input words val
-				Bson gt = Filters.gte("value", wordVal);
-				BsonDocument bsonDocument = gt.toBsonDocument(BsonDocument.class, com.mongodb.async.client.MongoClients.getDefaultCodecRegistry() );
-				JsonObject queryG = new JsonObject(bsonDocument.toJson());
-
-				// query for value that lower than input words val
-				Bson lt = Filters.lt("value", wordVal);
-				BsonDocument bsonDocumentl = lt.toBsonDocument(BsonDocument.class, com.mongodb.async.client.MongoClients.getDefaultCodecRegistry());
-				JsonObject queryL = new JsonObject(bsonDocumentl.toJson());
 
 				int finalWordVal = wordVal;
-				client.find(COLLECTION,  queryG, rG ->{
-					if(!rG.result().isEmpty()){
+				final FindOptions gtfindOptions = new FindOptions().setSort(new JsonObject().put("value",1)).setLimit(1);
+				final FindOptions ltfindOptions = new FindOptions().setSort(new JsonObject().put("value",-1)).setLimit(1);
+				JsonObject gtQuery = new JsonObject().put("value", new JsonObject().put("$gte",finalWordVal));
+				JsonObject ltQuery = new JsonObject().put("value", new JsonObject().put("$lt", finalWordVal));
 
-						rG.result().sort(new Comparator<JsonObject>() {
-							@Override
-							public int compare(JsonObject l, JsonObject r) {
-								int lf = Integer.parseInt(l.getValue("value").toString());
-								int ri = Integer.parseInt(r.getValue("value").toString());
-								return lf < ri ? -1 : lf > ri ? 1 : 0;
-							}
-						});
-
+				mongoclient.findWithOptions(COLLECTION, gtQuery ,gtfindOptions, rG -> {
+					if(!rG.result().isEmpty()){//found biggest value
 						System.out.println("found biggest value");
-						System.out.println(rG.result());
-
 						int disGt = Integer.parseInt(String.valueOf(rG.result().get(0).getValue("value"))) - finalWordVal;
 
-
-						client.find(COLLECTION,  queryL, rL ->{
-							if(!rL.result().isEmpty()) {
-								rL.result().sort(new Comparator<JsonObject>() {
-									@Override
-									public int compare(JsonObject l, JsonObject r) {
-										int lf = Integer.parseInt(l.getValue("value").toString());
-										int ri = Integer.parseInt(r.getValue("value").toString());
-										return lf > ri ? -1 : lf < ri ? 1 : 0;
-									}
-								});
+						mongoclient.findWithOptions(COLLECTION,  ltQuery, ltfindOptions, rL ->{
+							if(!rL.result().isEmpty()) {//found lowest value to
 								System.out.println("found lowest value to");
-								System.out.println(rL.result());
-
 								int disLt = finalWordVal - Integer.parseInt(String.valueOf(rL.result().get(0).getValue("value")));
-								if (disGt < disLt){
-									ans.put("value", rG.result().get(0).getValue("word"));
-									context.response().end(ans.toString()+"\n");
-									System.out.println("biggest value closer");
-								}else{
-									ans.put("value", rL.result().get(0).getValue("word"));
-									context.response().end(ans.toString()+"\n");
-									System.out.println("lowest value closer");
-								}
-							}else{
-								System.out.println("only biggest value");
-								ans.put("value", rG.result().get(0).getValue("word"));
-								context.response().end(ans.toString()+"\n");
-							}
+
+								if (disGt < disLt)//biggest value closer
+									ans[0] = getWord(rG);
+								else//lowest value closer
+									ans[0] = getWord(rL);
+
+							}else//only biggest value
+								ans[0] = getWord(rG);
+							lookForLexical(text,ans, context,finalWordVal);
 						});
 
 					}else{
-						client.find(COLLECTION,  queryL, rL ->{
-							if(!rL.result().isEmpty()) {
-								rL.result().sort(new Comparator<JsonObject>() {
-									@Override
-									public int compare(JsonObject l, JsonObject r) {
-										int lf = Integer.parseInt(l.getValue("value").toString());
-										int ri = Integer.parseInt(r.getValue("value").toString());
-										return lf > ri ? -1 : lf < ri ? 1 : 0;
-									}
-								});
-
-								ans.put("value", rL.result().get(0).getValue("word"));
-								context.response().end(ans.toString()+"\n");
-								System.out.println("only lowest value");
-								System.out.println(rL);
-							}else{
-								ans.put("value", "null");
-								context.response().end(ans.toString()+"\n");
-								System.out.println("the is no match!" + ans.toString());
-							}
+						mongoclient.findWithOptions(COLLECTION, ltQuery, ltfindOptions, rL ->{
+							if(!rL.result().isEmpty())
+								ans[0] = getWord(rL);
+							else
+								ans[0] = "null";
+							lookForLexical(text,ans,context,finalWordVal);
 						});
-					}
-				});
-
-				// insert the new word to the DB
-				client.insert("words", record, r -> {
-					if (r.succeeded()) {
-						//print on server
-						System.out.println("Saved word : " + record.getString("word") + ", with the value:" + record.getInteger("value"));
-					} else {
-						r.cause().printStackTrace();
 					}
 				});
 			}
-			// look for the closest by lexical
-
 		});
+	}
+
+	private void lookForLexical(String text, String[] ans, RoutingContext context, int finalWordVal) {
+		final FindOptions gtRegFindOptions = new FindOptions().setSort(new JsonObject().put("word", 1)).setLimit(1);
+		final FindOptions ltRegFindOptionslt = new FindOptions().setSort(new JsonObject().put("word", -1)).setLimit(1);
+		JsonObject gtRegQuery = new JsonObject().put("word", new JsonObject().put("$gte", text));
+		JsonObject ltRegQuery = new JsonObject().put("word", new JsonObject().put("$lt", text));
+
+		mongoclient.findWithOptions(COLLECTION, gtRegQuery, gtRegFindOptions, gtR -> {
+			if (!gtR.result().isEmpty()) {
+				String gWord = getWord(gtR);
+				mongoclient.findWithOptions(COLLECTION, ltRegQuery, ltRegFindOptionslt, ltR -> {
+					if (!ltR.result().isEmpty()) {
+						String lWord = getWord(ltR);
+						int compareGt = compareStrings(gWord, text);
+						int compareLt = compareStrings(text, lWord);
+						System.out.println(lWord + "(" + compareLt + "), " + text + "," + gWord + "(" + compareGt + ")");
+
+						if (compareLt < compareGt)
+							ans[1] = lWord;
+						else
+							ans[1] = gWord;
+
+					} else
+						ans[1] = gWord;
+					context.response().end(new JsonObject().put("value", ans[0]).put("lexical", ans[1]) + "\n");
+					insertToDB(text, finalWordVal);
+
+				});
+
+			} else {
+				mongoclient.findWithOptions(COLLECTION, ltRegQuery, ltRegFindOptionslt, ltR -> {
+					if (!ltR.result().isEmpty())
+						ans[1] = getWord(ltR);
+					else
+						ans[1] = "null";
+
+					context.response().end(new JsonObject().put("value", ans[0]).put("lexical", ans[1]) + "\n");
+					insertToDB(text, finalWordVal);
+
+				});
+			}
+		});
+		System.out.println("lexical results" + ans[1]);
+	}
+
+	private void insertToDB(String text, int val) {
+
+		mongoclient.insert("words", new JsonObject().put("word", text).put("value", val), r -> {
+			if (r.succeeded()) {
+				System.out.println("Saved word : " + text + ", with the value:" + val);
+			} else {
+				r.cause().printStackTrace();
+			}
+		});
+	}
+
+	private String getWord(AsyncResult<List<JsonObject>> h) {
+		return h.result().get(0).getValue("word").toString();
+	}
+
+	private static int compareStrings(String s1, String s2) {
+		int compareSum = 0;
+		int c1, c2;
+		for(int i = 0; i < s1.length() && i < s2.length(); i++) {
+			c1 = (int) s1.charAt(i);
+			c2 = (int) s2.charAt(i);
+			compareSum += c1 - c2;
+		}
+
+		if (s1.length() > s2.length()) {
+			for (int i = s2.length(); i < s1.length(); i++) {
+				compareSum += ((int) s1.charAt(i) - 96);
+			}
+			return Math.abs(compareSum);
+		}
+		if (s1.length() < s2.length()) {
+			for (int i = s1.length(); i < s2.length(); i++) {
+				compareSum += ((int) s2.charAt(i) - 96);
+			}
+			return Math.abs(compareSum);
+		}
+
+		return Math.abs(compareSum);
 	}
 
 }
